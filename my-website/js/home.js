5e942e73594a775b20ef71bb1b0a54b5692bbf9c
Damdamin91 <damsilongan@gmail.com>
2025-07-26 14:07:47 +0800

    home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'a31475ce1cd89df4b31e13e7af97f9c5';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
